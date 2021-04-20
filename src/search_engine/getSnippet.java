8f3214f12369dcaba564ef347780c924c73e5ef1
inulee <bapmukza9@gmail.com>
2021-04-20 18:57:04 +0900

    args읽기, 데이터 읽기, 공백으로 split, 사용자 입력 값 받기 구현

--- src/search_engine/getSnippet.java
+++ src/search_engine/getSnippet.java
@@ -1,10 +1,52 @@
 package search_engine;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
+import org.w3c.dom.Document;
+
+
 public class getSnippet {
 
 
 	public getSnippet(String txt, String keyword) {
-			
+		try {
+		String fileName = "src\\" + txt;
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.newDocument();
+		
+		Path filePath = Paths.get(fileName);
+		InputStream inputStream = Files.newInputStream(filePath);
+		org.jsoup.nodes.Document readDoc = Jsoup.parse(inputStream, "UTF-8", "", Parser.xmlParser());
+		
+		String total = readDoc.text();
+		
+		String data[] = total.split(" ");
+		
+		System.out.println("<" + txt + " 데이터 split>");
+		for(int i =0 ; i < data.length ; i++) {
+			System.out.println(data[i]);
+		}
+		
+		System.out.println("\n<사용자 입력 값>");
+		System.out.println(keyword + "\n");
+		
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 
