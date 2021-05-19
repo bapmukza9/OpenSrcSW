b2e64dd31896ef4fb07d7154177d3afb489d913f
inulee <bapmukza9@gmail.com>
2021-05-19 18:14:14 +0900

    < 파이썬 실습 (11주차) >
    week11.ipynb (11주차 실습)
    file1.txt
    file2.txt
    (소스 마지막 수정 날짜 : 2021/05/19)

--- src/search_engine/kuir.java
+++ src/search_engine/kuir.java
@@ -3,7 +3,7 @@ package search_engine;
 public class kuir {
 	public static void main(String[] args) {
 		
-		if(args.length == 2) {
+		if(args.length >= 2) {
 			if(args[0].equals("-c")) {	
 				makeCollection trigger = new makeCollection(args[1]);
 			}
@@ -12,6 +12,9 @@ public class kuir {
 			}
 			else if(args[0].equals("-i")) {
 				indexer trigger = new indexer(args[1]);
+			}
+			else if(args[0].equals("-s")) {
+				searcher trigger = new searcher(args[3], args[1]);
 			}
 			else {
 				System.out.println("\"" + args[0] + "\"는 없는 명령어입니다.");
